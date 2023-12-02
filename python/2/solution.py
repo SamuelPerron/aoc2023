546617b8882453760fd1e0835b3a90c0c04075fb
Samuel Perron <samuelperron@hotmail.fr>
2023-12-02 10:55:52 -0500

    Day 2 - Part 2 - Python

--- python/2/solution.py
+++ python/2/solution.py
@@ -23,6 +23,9 @@ class Game:
     def __init__(self, id, game_str):
         self.id = id
         self.cube_sets = self.parse_cube_sets(game_str)
+        self.min_red = self.min_number_of_color("red")
+        self.min_green = self.min_number_of_color("green")
+        self.min_blue = self.min_number_of_color("blue")
 
     def parse_cube_sets(self, game_str):
         cube_sets = []
@@ -42,23 +45,28 @@ class Game:
 
         return True
 
+    def min_number_of_color(self, color):
+        return max(
+            getattr(cube_set, color) for cube_set in self.cube_sets
+        )
+
+
+def parse_games(test):
+    games = []
+    with open(f"input{'.test' if test else ''}") as f:
+        i = 1
+        for line in f.read().splitlines():
+            game_str = line.split(": ")[1]
+            games.append(Game(i, game_str))
+            i += 1
+
+    return games
+
 
 class Solution:
     def __init__(self, bag, test):
         self.bag = bag
-        self.test = test
-        self.games = self.parse_games()
-
-    def parse_games(self):
-        games = []
-        with open(f"input{'.test' if self.test else ''}") as f:
-            i = 1
-            for line in f.read().splitlines():
-                game_str = line.split(": ")[1]
-                games.append(Game(i, game_str))
-                i += 1
-
-        return games
+        self.games = parse_games(test)
 
     def get_final_number(self):
         final_number = 0
@@ -69,9 +77,24 @@ class Solution:
         return final_number
 
 
+class SecondSolution:
+    def __init__(self, test):
+        self.games = parse_games(test)
+
+    def get_final_number(self):
+        final_number = 0
+        for game in self.games:
+            final_number += game.min_red * game.min_green * game.min_blue
+
+        return final_number
+
+
 if __name__ == "__main__":
     solution = Solution(Bag(12, 13, 14), test=False)
+    second_solution = SecondSolution(test=False)
+
     print(solution.get_final_number())
+    print(second_solution.get_final_number())
 
 
 
